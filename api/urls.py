4138ad49f4011ba07386d14f8385569141b7cd84
black15 <ousstalha77@gmail.com>
2022-11-05 17:07:22 +0100

    add post put delete request

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,11 @@ from .views import *
 
 urlpatterns = [
    path("authors", authors_list, name="authors"),
+   path("authors/<int:pk>", author_details, name="author_details"),
+
    path("books", books_list, name="books"),
+   path("books/<int:pk>", book_details, name="book_details"),
+
    path("categories", categories_list, name="categories"),
+   path("categories/<int:pk>", category_details, name="category_details"),
 ]
--- api/views.py
+++ api/views.py
@@ -7,12 +7,13 @@ from django.http.response import JsonResponse
 from .serializers import *
 
 
+# ------------------ Authors Views ------------------
 @api_view(('GET','POST'))
 def authors_list(request):
    if request.method == 'GET':
       authors     = Author.objects.all()
       serializer  = AuthorSerializer(authors, many=True)
-      return Response(serializer.data)
+      return Response(serializer.data, status=status.HTTP_200_OK)
 
    if request.method == 'POST':
       serializer     = AuthorSerializer(data=request.data)
@@ -20,7 +21,28 @@ def authors_list(request):
          serializer.save()
          return Response(serializer.data, status=status.HTTP_201_CREATED)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(('GET', 'PUT', 'DELETE'))
+def author_details(request, pk):
+   try:
+      author      =  Author.objects.get(pk=pk)
+   except Author.DoesNotExist:
+      return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+      
+   if request.method == 'GET':
+      serializer  = AuthorSerializer(author)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+   if request.method == 'PUT':
+      serializer  = AuthorSerializer(author, data=request.data)
+      if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+      return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+   if request.method == 'DELETE':
+      author.delete()
+      return Response({'message': True}, status=status.HTTP_204_NO_CONTENT)
 
+
+# ------------------ Books Views ------------------
 @api_view(('GET','POST'))
 def books_list(request):
    if request.method == 'GET':
@@ -35,6 +57,28 @@ def books_list(request):
          return Response(serializer.data, status=status.HTTP_201_CREATED)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(('GET', 'PUT', 'DELETE'))
+def book_details(request, pk):
+   try:
+      book      =  Book.objects.get(pk=pk)
+   except Book.DoesNotExist:
+      return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+      
+   if request.method == 'GET':
+      serializer  = BookSerializer(book)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+   if request.method == 'PUT':
+      serializer  = BookSerializer(book, data=request.data)
+      if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+      return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+   if request.method == 'DELETE':
+      book.delete()
+      return Response({'message': True}, status=status.HTTP_204_NO_CONTENT)
+
+
+# ------------------ Categories Views ------------------
 @api_view(('GET','POST'))
 def categories_list(request):
    if request.method == 'GET':
@@ -48,3 +92,23 @@ def categories_list(request):
          serializer.save()
          return Response(serializer.data, status=status.HTTP_201_CREATED)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(('GET', 'PUT', 'DELETE'))
+def category_details(request, pk):
+   try:
+      category      =  Category.objects.get(pk=pk)
+   except Category.DoesNotExist:
+      return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+      
+   if request.method == 'GET':
+      serializer  = CategorySerializer(category)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+   if request.method == 'PUT':
+      serializer  = CategorySerializer(category, data=request.data)
+      if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+      return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+   if request.method == 'DELETE':
+      category.delete()
+      return Response({'message': True}, status=status.HTTP_204_NO_CONTENT)
